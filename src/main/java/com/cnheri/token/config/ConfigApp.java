caf6fddc1b7fc959574aedebd2a52746911557c0
Richard <richardle1983@gmail.com>
2020-04-02 22:53:54 +0800

    update

--- src/main/java/com/cnheri/token/config/ConfigApp.java
+++ src/main/java/com/cnheri/token/config/ConfigApp.java
@@ -2,12 +2,12 @@ package com.cnheri.token.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApp.class, args);
